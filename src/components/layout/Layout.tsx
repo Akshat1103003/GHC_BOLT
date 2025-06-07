cf2abcd50c019009e691e15d87c5af55297e5ca2
Akshat Jain <120116012+Akshat1103003@users.noreply.github.com>
2025-06-07 21:17:37 +0530

    Remove Footer Component

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
-import Footer from './Footer';
 
 const Layout: React.FC = () => {
   return (
@@ -10,7 +9,6 @@ const Layout: React.FC = () => {
       <main className="flex-grow">
         <Outlet />
       </main>
-      <Footer />
     </div>
   );
 };
